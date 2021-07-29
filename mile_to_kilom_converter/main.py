10147d490901545e10950e6973d5b88bb1b14932
vaidehee <vaidehibhise94@gmail.com>
2021-07-29 10:40:16 -0700

    new converer

--- mile_to_kilom_converter/main.py
+++ mile_to_kilom_converter/main.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 def miles_kilo():
     miles = float(miles_input.get())
-    km = miles * 1.609
+    km = round(miles * 1.609)
     kilo_result_label.config(text=f"{km}")
 
 
