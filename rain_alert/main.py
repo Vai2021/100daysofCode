92edabf8827aa7ed2f867a820a221324e7c40aef
Vaidehee Bhise <vaideheebhise@Vaidehees-MacBook-Pro.local>
2021-08-18 17:15:12 -0700

    rain alert code

---
+++ rain_alert/main.py
@@ -0,0 +1,26 @@
+import requests
+OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = "f89cd919354a115d55e250e6e13c2619"
+
+weather_parameters={
+    "lat":19.075983,
+    "lon":72.877655,
+    "appid": api_key,
+    "exclude":"current,minutely,daily"
+}
+
+response= requests.get(OWM_endpoint,params=weather_parameters)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice= weather_data["hourly"][:12]
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code =  hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring Umbrella")
+#print(weather_data["hourly"][0]["weather"][0]["id"])
+
